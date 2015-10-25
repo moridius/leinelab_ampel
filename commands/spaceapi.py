1be17f604985a5c7df927efd05610d7c90d78e0e
lemoer <leo.moerlein@gmail.com>
2015-10-25 20:28:55 +0100

    Removed cruft.

--- commands/spaceapi.py
+++
@@ -1,7 +0,0 @@
-#!/home/leinelab/ampel/ve_python
-
-import urllib.request
-
-def TriggerAPI( openIt ):
-    #print( "TriggerAPI" )
-    urllib.request.urlopen( "https://leinelab.net/ampel/spaceapi.php?open=" + str( int( openIt ) ) )
