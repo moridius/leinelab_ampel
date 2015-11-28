5eeb0aace36cc47c8fd01d5f9dc532ed9271320c
lemoer <leo.moerlein@gmail.com>
2015-11-28 06:13:00 +0100

    Added push_webserver script. Say hello to it.

---
+++ push_webserver.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import time
+import foreman
+import logging
+import requests
+
+URL='http://leinelab.net/ampel/SetStatus.php?status=%s'
+
+if __name__ == '__main__':
+    FORMAT= '%(levelname)+8s %(message)s'
+    logging.basicConfig(format=FORMAT, level='INFO')
+    
+    last_status = None
+    
+    while True:
+        status = foreman.notify('Status')
+
+        if last_status != status:
+            logging.info('status changed: pushing %s to webserver.' % status)
+            requests.get(URL % status)
+
+        last_status = status
+        time.sleep(1)
