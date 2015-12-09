a802430a838742e6b377a1eafabb15445aabbd66
lemoer <leo.moerlein@gmail.com>
2015-12-09 05:45:51 +0100

    poll_webserver now uses requests lib.

--- poll_webserver.py
+++ poll_webserver.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-import urllib.request
-import datetime
+import requests
 import time
 import argparse
 import foreman
@@ -11,18 +10,20 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--log-level', type=str, default='INFO',
                         dest='log_level')
-    
+
     args = parser.parse_args()
 
-    FORMAT= '%(levelname)+8s %(message)s'
+    FORMAT = '%(levelname)+8s %(message)s'
     logging.basicConfig(format=FORMAT, level=args.log_level)
 
     while True:
-        res = urllib.request.urlopen("http://leinelab.net/ampel/GetJob.php")
-        content = res.read().decode()
+        res = requests.get("http://leinelab.net/ampel/GetJob.php")
+        content = res.text()
+
         if content != "":
             foreman.notify(content)
             logging.info("Polled webserver and got something.")
         else:
             logging.debug("Polled webserver, got nothing.")
-        time.sleep( 15 )
+
+        time.sleep(15)
